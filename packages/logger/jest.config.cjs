33ef2a7dea616b4f15c9c7e030ee85b0fe5d5ed9
Arno V <versini@gmail.com>
2023-05-09 13:47:04 -0400

    chore: moving Jest configuration to common folder

--- packages/logger/jest.config.cjs
+++ packages/logger/jest.config.cjs
@@ -1,28 +1,5 @@
+const commonJest = require("../../configuration/jest.config.common.cjs");
+
 module.exports = {
-	// coverageDirectory: "<rootDir>/../../coverage",
-	// roots: ["<rootDir>/../../packages/logger/src"],
-	transform: {
-		"^.+\\.(t|j)s?$": [
-			"@swc/jest",
-			{
-				exclude: [],
-				swcrc: false,
-				jsc: {
-					parser: {
-						syntax: "typescript",
-					},
-					externalHelpers: true,
-				},
-			},
-		],
-	},
-	collectCoverageFrom: ["src/*.{js,mjs,jsx,ts,tsx}"],
-	coverageThreshold: {
-		global: {
-			branches: 100,
-			functions: 100,
-			lines: 100,
-			statements: 100,
-		},
-	},
+	...commonJest,
 };
