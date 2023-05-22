71d19926b7edec71289d047835e71c4cba6aaef4
Arno V <versini@gmail.com>
2023-05-21 21:34:57 -0400

    moving deepEqual code to tests only scope

--- configuration/jest.config.common.cjs
+++ configuration/jest.config.common.cjs
@@ -16,6 +16,16 @@ module.exports = {
 		],
 	},
 	transformIgnorePatterns: ["/node_modules/?!(execa)/"],
+	/**
+	 * An array of glob patterns indicating a set of files for which
+	 * coverage information should be collected. If a file matches
+	 * the specified glob pattern, coverage information will be
+	 * collected for it even if no tests exist for this file and
+	 * it's never required in the test suite.
+	 * @type {Array}
+	 *
+	 * Default undefined
+	 */
 	collectCoverageFrom: ["src/*.{js,mjs,jsx,ts,tsx}"],
 	coverageThreshold: {
 		global: {
@@ -29,4 +39,13 @@ module.exports = {
 	moduleNameMapper: {
 		"(.+)\\.js": "$1",
 	},
+	/**
+	 * The glob patterns Jest uses to detect test files. By default
+	 * it looks for .js, .jsx, .ts and .tsx files inside of
+	 * __tests__ folders, as well as any files with a suffix of
+	 * .test or .spec (e.g. Component.test.js or Component.spec.js).
+	 * It will also find files called test.js or spec.js.
+	 * @type {Array}
+	 */
+	testMatch: ["**/__tests__/**/*.test.ts"],
 };
