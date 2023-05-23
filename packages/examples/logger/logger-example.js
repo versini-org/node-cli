9f8765d760ee72df63ab0c05e7b47c5f8a1f0347
Arno V <versini@gmail.com>
2023-05-23 10:51:47 -0400

    feat: adding printBox method

--- packages/examples/logger/logger-example.js
+++ packages/examples/logger/logger-example.js
@@ -2,8 +2,24 @@
 
 import { Logger } from "@node-cli/logger";
 
-const logger = new Logger();
-logger.timestamp = true;
-logger.info("hello world");
-logger.error("hello world");
-logger.warn("hello warning");
+const log = new Logger();
+log.timestamp = true;
+// log.boring = true;
+log.prefix = "==>";
+
+log.info("hello world");
+log.error("hello world");
+log.warn("hello warning");
+log.log();
+log.printBox(["hello world", "hello world hello world"], {
+	padding: 0,
+	textAlignment: "right",
+	title: "hello world box title",
+	width: 40,
+	// fullscreen: true,
+	newLineAfter: false,
+	margin: 1,
+	// borderColor: "red",
+});
+// log.log();
+// log.log("after the box");
