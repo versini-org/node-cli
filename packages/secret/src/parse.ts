f03c0a68ce643cb846f362a4d148844e823e7c1e
Arno V <versini@gmail.com>
2023-05-29 18:30:21 -0400

    feat: adding Secret package

---
+++ packages/secret/src/parse.ts
@@ -0,0 +1,88 @@
+import kleur from "kleur";
+import { parser } from "@node-cli/parser";
+
+export type Flags = {
+	decrypt?: boolean;
+	encrypt?: boolean;
+	help?: boolean;
+	version?: boolean;
+};
+
+export type Parameters = {
+	input?: string;
+	output?: string;
+};
+
+export type Configuration = {
+	flags?: Flags;
+	parameters?: Parameters;
+};
+
+/* istanbul ignore next */
+export const config: Configuration = parser({
+	meta: import.meta,
+	examples: [
+		{
+			command: 'secret -e "my-file.txt" "my-file.txt.enc"',
+			comment:
+				'## Encrypt the file "my-file.txt" and save the result in\n    ## "my-file.txt.enc" - password will be prompted',
+		},
+		{
+			command: 'secret -d "my-file.txt.enc" "my-file.txt"',
+			comment: `## Decrypt the file "my-file.txt.enc" and save the\n    ## result in "my-file.txt" - password will be prompted`,
+		},
+	],
+	flags: {
+		decrypt: {
+			shortFlag: "d",
+			description: `Decrypt a password protected file`,
+			type: "boolean",
+		},
+		encrypt: {
+			shortFlag: "e",
+			description: `Encrypt a file with a password`,
+			type: "boolean",
+		},
+		help: {
+			shortFlag: "h",
+			description: "Display help instructions",
+			type: "boolean",
+		},
+		version: {
+			shortFlag: "v",
+			description: "Output the current version",
+			type: "boolean",
+		},
+	},
+	parameters: {
+		input: {
+			description: "The file to encrypt or decrypt",
+		},
+		output: {
+			description:
+				"The file to create in order to save the result of the encryption or decryption",
+		},
+	},
+	restrictions: [
+		{
+			exit: 1,
+			message: () =>
+				kleur.red(
+					`\nError: one of --encrypt or --decrypt option must be provided.`
+				),
+			test: (x: { encrypt: boolean; decrypt: boolean }) =>
+				x.encrypt === false && x.decrypt === false,
+		},
+		{
+			exit: 1,
+			message: () =>
+				kleur.red(
+					`\nError: either --encrypt or --decrypt option must be provided, but not both.`
+				),
+			test: (x: { encrypt: boolean; decrypt: boolean }) =>
+				x.encrypt === true && x.decrypt === true,
+		},
+	],
+
+	usage: "secret [options] [input] [output]",
+});
---
+++ packages/secret/src/secret.ts
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+/* istanbul ignore file */
+
+import {
+	displayConfirmation,
+	displayPromptWithPassword,
+	logger,
+	processFileWithPassword,
+	shouldContinue,
+} from "./utilities.js";
+
+import { config } from "./parse.js";
+import fs from "fs-extra";
+import path from "node:path";
+
+const ENCRYPT = "encrypt";
+const DECRYPT = "decrypt";
+
+/**
+ * Caching the "action" for future usage (encrypt or decrypt).
+ */
+const actionName = config.flags.encrypt ? ENCRYPT : DECRYPT;
+
+/**
+ * Extracting the input and output files.
+ */
+let inputFile: string,
+	outputFile: string,
+	outputFileExists: boolean = false;
+if (Object.entries(config.parameters).length > 0) {
+	inputFile = config.parameters["0"];
+	outputFile = config.parameters["1"];
+	if (!fs.existsSync(inputFile)) {
+		logger.printErrorsAndExit([`File "${inputFile}" does not exist!`], 1);
+	}
+	if (fs.existsSync(outputFile)) {
+		outputFileExists = true;
+	}
+}
+
+if (outputFileExists) {
+	const goodToGo = await displayConfirmation(
+		`The file ${outputFile} already exists, overwrite it?`
+	);
+	shouldContinue(goodToGo);
+}
+
+const password = await displayPromptWithPassword(
+	`Enter password to ${actionName} the file`
+);
+
+try {
+	await processFileWithPassword({
+		encode: config.flags.encrypt,
+		input: inputFile,
+		output: outputFile,
+		password,
+	});
+	logger.log();
+	logger.info(`File ${path.basename(inputFile)} was ${actionName}ed.`);
+	if (outputFileExists) {
+		logger.info(`The result was saved in the file ${outputFile}`);
+	}
+} catch (error) {
+	logger.error(error);
+}
