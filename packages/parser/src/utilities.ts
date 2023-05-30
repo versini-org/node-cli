615e88e12ac65287646a277deb43c1b484bca57c
Arno V <versini@gmail.com>
2023-05-30 10:33:50 -0400

    refactor: use @node-cli/utilities and remove lodash dep
    Release-As: 2.2.1

--- packages/parser/src/utilities.ts
+++ packages/parser/src/utilities.ts
@@ -2,48 +2,11 @@
 
 import { Logger } from "@node-cli/logger";
 import Table from "cli-table3";
-import _ from "lodash";
 import kleur from "kleur";
 import path from "node:path";
+import { upperFirst } from "@node-cli/utilities";
+
 const logger = new Logger();
-
-/**
- * Converts the first character of string to upper case
- * @param {string} string_ the string to convert
- * @returns {string} the converted string
- */
-const upperFirst = (string_: string): string =>
-	string_[0].toUpperCase() + string_.slice(1);
-
-/**
- * Wrapper method for lodash `merge()` and `mergeWith()` methods.
- *
- * Without the `customizer` function, this method recursively merges own and inherited
- * enumerable string keyed properties of source objects into the destination object.
- * Source properties that resolve to undefined are skipped if a destination value exists.
- * Array and plain object properties are merged recursively. Other objects and value
- * types are overridden by assignment. Source objects are applied from left to right.
- * Subsequent sources overwrite property assignments of previous sources.
- *
- * With the `customizer` function, the behavior is the same except that `customizer` is
- * invoked to produce the merged values of the destination and source properties.
- * If customizer returns undefined, merging is handled by the `shallowMerge` instead.
- * The customizer is invoked with six arguments: `(objValue, srcValue, key, object,
- * source, stack)`
- * @param {object} objectA
- * @param {object} objectB
- * @param {function} customizer
- * @returns {object} !! WARNING: this method will mutate objectA
- */
-export const shallowMerge = (
-	objectA: any,
-	objectB: any,
-	customizer?: any
-): object => {
-	return typeof customizer === "function"
-		? _.mergeWith(objectA, objectB, customizer)
-		: _.merge(objectA, objectB);
-};
 
 export const meowOptionsHelper = (parameters_: {
 	flags?: any;
