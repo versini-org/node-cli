81b3a68d1acbf54df7540582d351388bdfca112b
Arno V <versini@gmail.com>
2024-02-11 12:00:55 -0500

    fix(envtools): ensure .envtools exists

--- packages/envtools/shell/functions/banner.sh
+++ packages/envtools/shell/functions/banner.sh
@@ -5,42 +5,38 @@ function printBanner {
   if [ "$ATOM_TERMINAL" = "" ] || [ "$VSCODE_CLI" = "" ]; then
     if [ "$EVTLS_INIT_PARAM" != "reload" ]; then
       local HEADER="┌─────────────────────────────────────┐"
+      local FOOTER="└─────────────────────────────────────┘"
+      local SEP="│"
       local MAX_STRING="$HEADER"
       local MAX_SIZE=${#MAX_STRING}
-      # echo "MAX_SIZE: $MAX_SIZE"
 
-      if [ -f "${EVTLS_RUNTIME_DIR}/envtools-banner" ]; then
-        txtCyan "$HEADER" "nl"
-        txtCyan "│"
-        if isMac; then
-          txtDefault "       "
-          txtRed "★"
-          txtDefault " Welcome to Envtools "
-          txtRed "★"
-          txtDefault "       "
-        else
-          txtDefault "         Welcome to Envtools         "
-        fi
-        txtCyan "│" "nl"
-
-        if [ "$EVTLS_VERSION" != "" ]; then
-          local VERSION_STRING="v${EVTLS_VERSION}"
-          local VERSION_SIZE=${#VERSION_STRING}
-          local PADDING=$(($MAX_SIZE - $VERSION_SIZE - 3))
-          # echo "VERSION_SIZE: $VERSION_SIZE"
-          # echo "PADDING: $PADDING"
-
-          txtCyan "│                                     │" "nl"
-          txtCyan "│"
-          printf %${PADDING}s |tr " " " "
-          txtDefault "$VERSION_STRING "
-          txtCyan "│" "nl"
-        else
-          txtCyan "│                                     │" "nl"
-        fi
-
-        txtCyan "└─────────────────────────────────────┘" "nl"
+      txtCyan "$HEADER" "nl"
+      txtCyan "$SEP"
+      if isMac; then
+        txtDefault "       "
+        txtRed "★"
+        txtDefault " Welcome to Envtools "
+        txtRed "★"
+        txtDefault "       "
+      else
+        txtDefault "         Welcome to Envtools         "
       fi
+      txtCyan "$SEP" "nl"
+
+      if [ "$EVTLS_VERSION" != "" ]; then
+        local VERSION_STRING="v${EVTLS_VERSION}"
+        local VERSION_SIZE=${#VERSION_STRING}
+        local PADDING=$(($MAX_SIZE - $VERSION_SIZE - 3))
+
+        txtCyan "$SEP                                     $SEP" "nl"
+        txtCyan "$SEP"
+        printf %${PADDING}s | tr " " " "
+        txtDefault "$VERSION_STRING "
+        txtCyan "$SEP" "nl"
+      else
+        txtCyan "$SEP                                     $SEP" "nl"
+      fi
+      txtCyan "$FOOTER" "nl"
     fi
   fi
 }
--- packages/envtools/shell/functions/logs.sh
+++ packages/envtools/shell/functions/logs.sh
@@ -96,4 +96,3 @@ function txtColor {
     fi
   fi
 }
-
