de77e8943fff9d31ae9259284e62d042ac8af10d
Arno V <versini@gmail.com>
2024-02-14 11:43:43 -0500

    Update utilities.ts

--- packages/npmrc/src/utilities.ts
+++ packages/npmrc/src/utilities.ts
@@ -41,58 +41,49 @@ export const listProfiles = async ({ flags, storeConfig }) => {
 };
 
 export const createProfile = async ({
-	flags,
 	storeConfig,
 	storeLocation,
 	profileName,
 	homeLocation,
 }) => {
+	let profiles = { available: [], enabled: undefined };
+	await fs.ensureFile(storeConfig);
 	try {
-		let profiles = { available: [], enabled: undefined };
-		await fs.ensureFile(storeConfig);
-		try {
-			// if the profile already exists, do nothing
-			profiles = await fs.readJson(storeConfig);
-			if (profiles.available.includes(profileName)) {
-				logger.warn(`Profile '${profileName}' already exists...`);
-				return 0;
-			}
-		} catch {
-			// ignoring error since we are creating the file
+		// if the profile already exists, do nothing
+		profiles = await fs.readJson(storeConfig);
+		if (profiles.available.includes(profileName)) {
+			logger.warn(`Profile '${profileName}' already exists...`);
+			return 0;
 		}
-
-		// if the profile does not exist, create
-		// a folder named as the profile under the storeLocation folder,
-		// with the existing npmrc / yarnrc files
-		await fs.ensureDir(`${storeLocation}/${profileName}`);
-		// copy the existing npmrc / yarnrc files to the new folder
-		const NPMRC = `${homeLocation}/.npmrc`;
-		const YARNRC = `${homeLocation}/.yarnrc`;
-		if (await fs.pathExists(YARNRC)) {
-			await fs.copy(YARNRC, `${storeLocation}/${profileName}/yarnrc`);
-		}
-		if (await fs.pathExists(NPMRC)) {
-			await fs.copy(NPMRC, `${storeLocation}/${profileName}/npmrc`);
-		}
-		// then add the profile to the configuration file
-		const newProfiles = {
-			available: [...profiles.available, profileName],
-			enabled: profiles.enabled,
-		};
-		await fs.writeJson(storeConfig, newProfiles, { spaces: 2 });
-		logger.printBox(`Profile '${profileName}' created`, {
-			textAlignment: "left",
-			title: "Profiles",
-			borderColor: "blue",
-		});
-		return 0;
-	} catch (error) {
-		if (flags.verbose) {
-			logger.log(error);
-		}
-		logger.error("Could not create profile");
-		return 1;
+	} catch {
+		// ignoring error since we are creating the file
 	}
+
+	// if the profile does not exist, create
+	// a folder named as the profile under the storeLocation folder,
+	// with the existing npmrc / yarnrc files
+	await fs.ensureDir(`${storeLocation}/${profileName}`);
+	// copy the existing npmrc / yarnrc files to the new folder
+	const NPMRC = `${homeLocation}/.npmrc`;
+	const YARNRC = `${homeLocation}/.yarnrc`;
+	if (await fs.pathExists(YARNRC)) {
+		await fs.copy(YARNRC, `${storeLocation}/${profileName}/yarnrc`);
+	}
+	if (await fs.pathExists(NPMRC)) {
+		await fs.copy(NPMRC, `${storeLocation}/${profileName}/npmrc`);
+	}
+	// then add the profile to the configuration file
+	const newProfiles = {
+		available: [...profiles.available, profileName],
+		enabled: profiles.enabled ?? profileName,
+	};
+	await fs.writeJson(storeConfig, newProfiles, { spaces: 2 });
+	logger.printBox(`Profile '${profileName}' created`, {
+		textAlignment: "left",
+		title: "Profiles",
+		borderColor: "blue",
+	});
+	return 0;
 };
 
 export const switchProfile = async ({
