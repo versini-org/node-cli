dbce2e3ab8f0efe775dff56bfa62863030f58749
Arno V <versini@gmail.com>
2024-03-22 09:44:35 -0400

    fix(npmrc): better registry listing formatting (#291)

---
+++ packages/npmrc/src/utilities/common.ts
@@ -0,0 +1,45 @@
+import kleur from "kleur";
+
+export const GET_REGISTRY_CMD =
+	'npm config list -l -g -json | grep registry\\"';
+
+export const formatRegistries = (registries: string) => {
+	/**
+	 * We receive this kind of output:
+	 * [
+	 *   '  "@node-cli:registry": "https://other-registry.npmjs.org/"',
+	 *   '  "registry": "https://registry.npmjs.org/"',
+	 * ]
+	 * And we need convert that to a JSON object that looks like that:
+	 * [
+	 *   { "@node-cli:registry": "https://other-registry.npmjs.org/" },
+	 *   { "registry": "https://registry.npmjs.org/" },
+	 * ]
+	 */
+	const messages = [];
+	const stdoutLines = registries.split("\n");
+	const jsonObjects = stdoutLines
+		.map((line) => {
+			const trimmedLine = line.trim();
+			if (trimmedLine.startsWith('"') && trimmedLine.endsWith(",")) {
+				const jsonLine = trimmedLine.slice(0, -1); // remove trailing comma
+				try {
+					const jsonObject = JSON.parse(`{${jsonLine}}`);
+					return jsonObject;
+				} catch {
+					// nothing to declare officer
+				}
+			}
+			// eslint-disable-next-line unicorn/no-null
+			return null;
+		})
+		.filter(Boolean); // remove undefined values
+
+	for (const jsonObject of jsonObjects) {
+		messages.push(
+			` • ${Object.keys(jsonObject)}:`,
+			`    ${kleur.underline().grey(`${Object.values(jsonObject)}`)}`,
+		);
+	}
+	return messages;
+};
